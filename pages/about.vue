5973624da8afc865522dc30d20f2e725785b1dda
Abel Masila <abel@x2ai.com>
2022-11-11 00:28:10 +0300

    setup nuxt 3

---
+++ pages/about.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat, quidem
+      quod placeat sint veritatis aperiam nostrum maiores laborum. Harum
+      aspernatur ab, obcaecati vitae voluptate consequuntur atque autem
+      veritatis debitis sequi?
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
