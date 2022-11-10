c77f8efb6c0de2f2d29a5a150a744a1d9eed46a8
Abel Masila <abel@x2ai.com>
2022-11-11 00:35:18 +0300

    init folder setup

---
+++ pages/products/[id].vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h5>Product ID: {{ id }}</h5>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente nobis
+      quae libero eum exercitationem dolore sunt architecto pariatur provident
+      voluptatum! Facilis vel laborum ad iusto? Natus quasi itaque commodi
+      beatae!
+    </p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params;
+</script>
+
+<style scoped></style>
---
+++ pages/products/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h5>Products</h5>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
